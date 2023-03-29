3a6b34b094a1e32ed83e083f68cd5462c2db520f
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-03-29 04:42:43 +0100

    Set Up

---
+++ script.js
@@ -0,0 +1,5 @@
+var myName = "Leila Paria";
+console.log(myName);
+var inputs = document.querySelectorAll("input");
+console.log(inputs);
+inputs.forEach(function (input) { return console.log(input); });
---
+++ script.ts
@@ -0,0 +1,7 @@
+const myName = "Leila Paria";
+console.log(myName);
+
+const inputs = document.querySelectorAll("input");
+console.log(inputs);
+
+inputs.forEach((input) => console.log(input));
