79119dc0d9a1f0e81e5e378133f9b4608518830e
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-03-30 13:18:36 +0100

    using any

--- script.ts
+++ script.ts
@@ -120,3 +120,34 @@ name:"Lily",
 } //initilization
 //if I add another property in the object above when initilizing I get 
 //error as there is not in the object when declaring.
+
+//any
+//If I want to have any possible different types when initializing later,
+// we can use any, however we should be aware that we might face same kind of issues in JS.
+
+let age: any;
+
+age = 25;
+console.log(age);
+
+age = "mahdi";
+console.log(age);
+
+age = true;
+console.log(age);
+
+let mixed: any[] = [];
+
+mixed.push(25);
+mixed.push("mahdi");
+mixed.push(false);
+
+console.log(mixed);
+
+let person: { name: any; age: any };
+
+person = { name: "mahdi", age: 25 };
+console.log(person);
+
+person = { name: 25, age: "mahdi" }; //is OK here but it might makes our app become down or etc.
+console.log(person);
